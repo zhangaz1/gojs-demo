0d62ea9fdd8a085cc1991c40ccc3861256261f06
zhangaz1 <zhangaz1@hotmail.com>
2016-06-27 17:49:44 +0800

    画出猫头

--- src/js/learn-demo.js
+++ src/js/learn-demo.js
@@ -1,26 +1,55 @@
 $(function() {
-    var option = {
-        initialContentAlignment: go.Spot.Center,
-        'undoManager.isEnabled': true
-    };
+    var $ = go.GraphObject.make;
+    var myDiagram =
+        $(go.Diagram, "myDiagramDiv", {
+            initialContentAlignment: go.Spot.Center, // center Diagram contents
+            "undoManager.isEnabled": true // enable Ctrl-Z to undo and Ctrl-Y to redo
+        });
 
-    var make = go.GraphObject.make;
-    var myDiagram = make(
-        go.Diagram,
-        'myDiagramDiv',
-        option
-    );
+    // define a simple Node template
+    myDiagram.nodeTemplate =
+        $(go.Node, "Horizontal",
+            // the entire node will have a light-blue background
+            {
+                background: "#44CCFF"
+            },
+            $(go.Picture,
+                // Pictures should normally have an explicit width and height.
+                // This picture has a red background, only visible when there is no source set
+                // or when the image is partially transparent.
+                {
+                    margin: 10,
+                    width: 50,
+                    height: 50,
+                    background: "red"
+                },
+                // Picture.source is data bound to the "source" attribute of the model data
+                new go.Binding("source")),
+            $(go.TextBlock,
+                "Default Text", // the initial value for TextBlock.text
+                // some room around the text, a larger font, and a white stroke:
+                {
+                    margin: 12,
+                    stroke: "white",
+                    font: "bold 16px sans-serif"
+                },
+                // TextBlock.text is data bound to the "name" attribute of the model data
+                new go.Binding("text", "name"))
+        );
 
-    var myModel = make(go.Model);
-
-    myModel.nodeDataArray = [{
-        key: 'Alpha'
-    }, {
-        key: 'Beta'
-    }, {
-        key: 'Gamma'
-    }];
-
-    myDiagram.model = myModel;
-
+    var model = $(go.Model);
+    model.nodeDataArray = [ // note that each node data object holds whatever properties it needs;
+        // for this app we add the "name" and "source" properties
+        {
+            name: "Don Meow",
+            source: "imgs/cat1.png"
+        }, {
+            name: "Copricat",
+            source: "imgs/cat2.png"
+        }, {
+            name: "Demeter",
+            source: "imgs/cat3.png"
+        }, { /* Empty node data */ }
+    ];
+    myDiagram.model = model;
 });
