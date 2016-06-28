c5bbd709cb3ff2b09a336f8bd58274156b930635
zhangaz1 <zhangaz1@hotmail.com>
2016-06-28 17:06:34 +0800

    使用part

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -2,62 +2,29 @@ $(function() {
     // For conciseness. See the "Building Parts" intro page for more
     var $ = go.GraphObject.make;
 
-    var diagram = $(go.Diagram, 'myDiagramDiv', {
-        layout: $(go.LayeredDigraphLayout)
-    });
+    var diagram = new go.Diagram('myDiagramDiv');
 
-    // the node template describes how each Node should be constructed
-    diagram.nodeTemplate =
-        $(go.Node, "Auto", // the Shape automatically fits around the TextBlock
-            $(go.Shape, "RoundedRectangle", // use this kind of figure for the Shape
-                // bind Shape.fill to Node.data.color
-                new go.Binding("fill", "color")),
+    diagram.add(
+        $(go.Part, "Vertical", {
+                margin: 100
+            },
             $(go.TextBlock, {
-                    margin: 3
-                }, // some room around the text
-                // bind TextBlock.text to Node.data.key
-                new go.Binding("text", "key"))
-        );
-
-    // the Model holds only the essential information describing the diagram
-    diagram.model = new go.GraphLinksModel(
-        [ // a JavaScript Array of JavaScript objects, one per node;
-            // the "color" property is added specifically for this app
-            {
-                key: "Alpha",
-                color: "lightblue"
-            }, {
-                key: "Beta",
-                color: "orange"
-            }, {
-                key: "Gamma",
-                color: "lightgreen"
-            }, {
-                key: "Delta",
-                color: "pink"
-            }
-        ], [ // a JavaScript Array of JavaScript objects, one per link
-            {
-                from: "Alpha",
-                to: "Beta"
-            }, {
-                from: "Alpha",
-                to: "Gamma"
-            }, {
-                from: "Beta",
-                to: "Beta"
-            }, {
-                from: "Gamma",
-                to: "Delta"
-            }, {
-                from: "Delta",
-                to: "Alpha"
-            }
-        ]);
-
-    diagram.initialContentAlignment = go.Spot.Center;
-    // enable Ctrl-Z to undo and Ctrl-Y to redo
-    diagram.undoManager.isEnabled = true;
+                text: "a Text Block",
+                margin: 100
+            }),
+            $(go.TextBlock, {
+                text: "a Text Block",
+                stroke: "red"
+            }),
+            $(go.TextBlock, {
+                text: "a Text Block",
+                background: "lightblue"
+            }),
+            $(go.TextBlock, {
+                text: "a Text Block",
+                font: "bold 14pt serif"
+            })
+        ));
 
     window.g = {
         make: $,
