f0d047478d2621eb3b9affeaa98bd8f937387d9b
zhangaz1 <zhangaz1@hotmail.com>
2016-06-28 18:06:09 +0800

    形状的角度和缩放

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -6,89 +6,43 @@ $(function() {
         initialContentAlignment: go.Spot.Center
     });
 
-    var W_geometry = go.Geometry.parse(
-        "M 0,0 L 10,50 20,10 30,50 40,0", false);
-
     diagram.add(
-        $(go.Part, "Horizontal",
+        $(go.Part, "Table",
             $(go.Shape, {
-                geometry: W_geometry,
-                strokeWidth: 2
+                row: 0,
+                column: 1,
+                figure: "Club",
+                fill: "green",
+                width: 40,
+                height: 40,
+            }), // default angle is zero; default scale is one
+            $(go.Shape, {
+                row: 0,
+                column: 2,
+                figure: "Club",
+                fill: "green",
+                width: 40,
+                height: 40,
+                angle: -90
             }),
             $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "blue",
-                strokeWidth: 10,
-                strokeJoin: "miter",
-                strokeCap: "butt"
+                row: 0,
+                column: 3,
+                figure: "Club",
+                fill: "green",
+                width: 40,
+                height: 40,
+                scale: 5
             }),
             $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "blue",
-                strokeWidth: 10,
-                strokeJoin: "miter",
-                strokeCap: "round"
-            }),
-            $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "blue",
-                strokeWidth: 10,
-                strokeJoin: "miter",
-                strokeCap: "square"
-            }),
-            $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "green",
-                strokeWidth: 10,
-                strokeJoin: "bevel",
-                strokeCap: "butt"
-            }),
-            $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "green",
-                strokeWidth: 10,
-                strokeJoin: "bevel",
-                strokeCap: "round"
-            }),
-            $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "green",
-                strokeWidth: 10,
-                strokeJoin: "bevel",
-                strokeCap: "square"
-            }),
-            $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "red",
-                strokeWidth: 10,
-                strokeJoin: "round",
-                strokeCap: "butt"
-            }),
-            $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "red",
-                strokeWidth: 10,
-                strokeJoin: "round",
-                strokeCap: "round"
-            }),
-            $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "red",
-                strokeWidth: 10,
-                strokeJoin: "round",
-                strokeCap: "square"
-            }),
-            $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "purple",
-                strokeWidth: 2,
-                strokeDashArray: [4, 2]
-            }),
-            $(go.Shape, {
-                geometry: W_geometry,
-                stroke: "purple",
-                strokeWidth: 2,
-                strokeDashArray: [6, 6, 2, 2]
+                row: 0,
+                column: 4,
+                figure: "Club",
+                fill: "green",
+                width: 40,
+                height: 40,
+                angle: 30,
+                scale: 1.5
             })
         ));
 
