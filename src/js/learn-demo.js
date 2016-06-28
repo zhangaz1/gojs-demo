ab641d3330b7309e51b1013c51bd07233dbea0b4
zhangaz1 <zhangaz1@hotmail.com>
2016-06-28 15:23:00 +0800

    Link模板

--- src/js/learn-demo.js
+++ src/js/learn-demo.js
@@ -3,7 +3,12 @@ $(function() {
     var myDiagram =
         $(go.Diagram, "myDiagramDiv", {
             initialContentAlignment: go.Spot.Center, // center Diagram contents
-            "undoManager.isEnabled": true // enable Ctrl-Z to undo and Ctrl-Y to redo
+            "undoManager.isEnabled": true, // enable Ctrl-Z to undo and Ctrl-Y to redo
+            layout: $(go.TreeLayout, // specify a Diagram.layout that arranges trees
+                {
+                    angle: 90,
+                    layerSpacing: 35
+                })
         });
 
     // the template we defined earlier
@@ -27,51 +32,62 @@ $(function() {
         );
 
     var model = $(go.TreeModel);
-    model.nodeDataArray = [ // the "key" and "parent" property names are required,
-        // but you can add whatever data properties you need for your app
-        {
-            key: "2",
-            parent: "1",
-            name: "Demeter",
-            source: "imgs/cat2.png"
-        }, {
-            key: "3",
-            parent: "1",
-            name: "Copricat",
-            source: "imgs/cat3.png"
-        }, {
-            key: "4",
-            parent: "3",
-            name: "Jellylorum",
-            source: "imgs/cat4.png"
-        }, {
-            key: "5",
-            parent: "3",
-            name: "Alonzo",
-            source: "imgs/cat5.png"
-        }, {
-            key: "1",
-            name: "Don Meow",
-            source: "imgs/cat1.png"
-        }, {
-            key: "6",
-            parent: "2",
-            name: "Munkustrap",
-            source: "imgs/cat6.png"
-        }
-    ];
-
-    myDiagram.layout =
-        $(go.TreeLayout, {
-            angle: 270,
-            layerSpacing: 70
-        });
-
+    model.nodeDataArray = [{
+        key: "1",
+        name: "Don Meow",
+        source: "imgs/cat1.png"
+    }, {
+        key: "2",
+        parent: "1",
+        name: "Demeter",
+        source: "imgs/cat2.png"
+    }, {
+        key: "3",
+        parent: "1",
+        name: "Copricat",
+        source: "imgs/cat3.png"
+    }, {
+        key: "4",
+        parent: "3",
+        name: "Jellylorum",
+        source: "imgs/cat4.png"
+    }, {
+        key: "5",
+        parent: "3",
+        name: "Alonzo",
+        source: "imgs/cat5.png"
+    }, {
+        key: "6",
+        parent: "2",
+        name: "Munkustrap",
+        source: "imgs/cat6.png"
+    }];
     myDiagram.model = model;
+    // define a Link template that routes orthogonally, with no arrowhead
+
+    var linkTemplate =
+        myDiagram.linkTemplate =
+        $(go.Link,
+            // default routing is go.Link.Normal
+            // default corner is 0
+            {
+                routing: go.Link.Orthogonal,
+                corner: 5
+            },
+            $(go.Shape, {
+                strokeWidth: 3,
+                stroke: "#555"
+            }) // the link shape
+
+            // if we wanted an arrowhead we would also add another Shape with toArrow defined:
+            // $(go.Shape, { toArrow: "Standard", stroke: null }
+        );
+
 
     window.g = {
         make: $,
         diagram: myDiagram,
-        model: model
+        model: model,
+        linkTemplate: linkTemplate
     };
 });
