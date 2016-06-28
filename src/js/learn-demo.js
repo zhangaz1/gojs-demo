1dd842b4bea8519449441c3c0411c6ca4cd0e144
zhangaz1 <zhangaz1@hotmail.com>
2016-06-28 15:12:23 +0800

    猫家族谱

--- src/js/learn-demo.js
+++ src/js/learn-demo.js
@@ -6,26 +6,64 @@ $(function() {
             "undoManager.isEnabled": true // enable Ctrl-Z to undo and Ctrl-Y to redo
         });
 
-    var model = $(go.GraphLinksModel);
-    model.nodeDataArray = [{
-        key: "A"
-    }, {
-        key: "B"
-    }, {
-        key: "A"
-    }];
-    model.linkDataArray = [{
-        from: "A",
-        to: "B"
-    }, {
-        from: "B",
-        to: "A"
-    }];
+    // the template we defined earlier
+    myDiagram.nodeTemplate =
+        $(go.Node, "Horizontal", {
+                background: "#44CCFF"
+            },
+            $(go.Picture, {
+                    margin: 10,
+                    width: 50,
+                    height: 50,
+                    background: "red"
+                },
+                new go.Binding("source")),
+            $(go.TextBlock, "Default Text", {
+                    margin: 12,
+                    stroke: "white",
+                    font: "bold 16px sans-serif"
+                },
+                new go.Binding("text", "name"))
+        );
 
+    var model = $(go.TreeModel);
+    model.nodeDataArray = [ // the "key" and "parent" property names are required,
+        // but you can add whatever data properties you need for your app
+        {
+            key: "2",
+            parent: "1",
+            name: "Demeter",
+            source: "imgs/cat2.png"
+        }, {
+            key: "3",
+            parent: "1",
+            name: "Copricat",
+            source: "imgs/cat3.png"
+        }, {
+            key: "4",
+            parent: "3",
+            name: "Jellylorum",
+            source: "imgs/cat4.png"
+        }, {
+            key: "5",
+            parent: "3",
+            name: "Alonzo",
+            source: "imgs/cat5.png"
+        }, {
+            key: "1",
+            name: "Don Meow",
+            source: "imgs/cat1.png"
+        }, {
+            key: "6",
+            parent: "2",
+            name: "Munkustrap",
+            source: "imgs/cat6.png"
+        }
+    ];
     myDiagram.model = model;
 
     window.g = {
-        make: make,
+        make: $,
         diagram: myDiagram,
         model: model
     };
