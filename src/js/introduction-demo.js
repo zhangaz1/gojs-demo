6bddca9af9a1de77c00adfbfeafb60328c607baa
zhangaz1 <zhangaz1@hotmail.com>
2016-06-28 17:08:51 +0800

    设置textBlock的样式

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -2,27 +2,44 @@ $(function() {
     // For conciseness. See the "Building Parts" intro page for more
     var $ = go.GraphObject.make;
 
-    var diagram = new go.Diagram('myDiagramDiv');
+    var diagram = $(go.Diagram, 'myDiagramDiv', {
+        initialContentAlignment: go.Spot.Center
+    });
 
     diagram.add(
-        $(go.Part, "Vertical", {
-                margin: 100
-            },
+        $(go.Part, "Vertical",
             $(go.TextBlock, {
                 text: "a Text Block",
-                margin: 100
+                background: "lightgreen",
+                margin: 2
             }),
             $(go.TextBlock, {
                 text: "a Text Block",
-                stroke: "red"
+                background: "lightgreen",
+                margin: 2,
+                width: 100,
+                height: 33
             }),
             $(go.TextBlock, {
                 text: "a Text Block",
-                background: "lightblue"
+                background: "lightgreen",
+                margin: 2,
+                width: 60,
+                height: 33
             }),
             $(go.TextBlock, {
                 text: "a Text Block",
-                font: "bold 14pt serif"
+                background: "lightgreen",
+                margin: 2,
+                width: 50,
+                height: 22
+            }),
+            $(go.TextBlock, {
+                text: "a Text Block",
+                background: "lightgreen",
+                margin: 2,
+                width: 40,
+                height: 9
             })
         ));
 
