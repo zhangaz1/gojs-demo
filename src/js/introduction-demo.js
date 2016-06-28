1ceb4557fa3145a50d323f72184298b3f7fb1814
zhangaz1 <zhangaz1@hotmail.com>
2016-06-28 17:12:28 +0800

    使用文本对齐方式：textAlign

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -7,34 +7,55 @@ $(function() {
     });
 
     diagram.add(
-        $(go.Part, "Vertical",
-            $(go.TextBlock, {
-                text: "a Text Block",
-                background: "lightgreen",
-                margin: 2
-            }),
-            $(go.TextBlock, {
-                text: "a Text Block",
-                background: "lightgreen",
-                margin: 2,
-                width: 50,
-                wrap: go.TextBlock.None
-            }),
-            $(go.TextBlock, {
-                text: "a Text Block",
-                background: "lightgreen",
-                margin: 2,
-                width: 50,
-                wrap: go.TextBlock.WrapDesiredSize
-            }),
-            $(go.TextBlock, {
-                text: "a Text Block",
-                background: "lightgreen",
-                margin: 2,
-                width: 50,
-                wrap: go.TextBlock.WrapFit
-            })
+        $(go.Part, "Horizontal",
+            $(go.Panel, "Vertical", {
+                    width: 150,
+                    defaultStretch: go.GraphObject.Horizontal
+                },
+                $(go.TextBlock, {
+                    text: "textAlign: 'left'",
+                    background: "lightgreen",
+                    margin: 2,
+                    textAlign: "left"
+                }),
+                $(go.TextBlock, {
+                    text: "textAlign: 'center'",
+                    background: "lightgreen",
+                    margin: 2,
+                    textAlign: "center"
+                }),
+                $(go.TextBlock, {
+                    text: "textAlign: 'right'",
+                    background: "lightgreen",
+                    margin: 2,
+                    textAlign: "right"
+                })
+            ),
+            $(go.Panel, "Vertical", {
+                    width: 150,
+                    defaultStretch: go.GraphObject.None
+                },
+                $(go.TextBlock, {
+                    text: "alignment: Left",
+                    background: "lightgreen",
+                    margin: 2,
+                    alignment: go.Spot.Left
+                }),
+                $(go.TextBlock, {
+                    text: "alignment: Center",
+                    background: "lightgreen",
+                    margin: 2,
+                    alignment: go.Spot.Center
+                }),
+                $(go.TextBlock, {
+                    text: "alignment: Right",
+                    background: "lightgreen",
+                    margin: 2,
+                    alignment: go.Spot.Right
+                })
+            )
         ));
+
     window.g = {
         make: $,
         diagram: diagram
