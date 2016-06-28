dba91c35ee0d92502615a43375e8241db57189bc
zhangaz1 <zhangaz1@hotmail.com>
2016-06-28 17:42:20 +0800

    使用textBlock自适应

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -7,53 +7,35 @@ $(function() {
     });
 
     diagram.add(
-        $(go.Part, "Horizontal",
-            $(go.Panel, "Vertical", {
-                    width: 150,
-                    defaultStretch: go.GraphObject.Horizontal
-                },
-                $(go.TextBlock, {
-                    text: "textAlign: 'left'",
-                    background: "lightgreen",
-                    margin: 2,
-                    textAlign: "left"
-                }),
-                $(go.TextBlock, {
-                    text: "textAlign: 'center'",
-                    background: "lightgreen",
-                    margin: 2,
-                    textAlign: "center"
-                }),
-                $(go.TextBlock, {
-                    text: "textAlign: 'right'",
-                    background: "lightgreen",
-                    margin: 2,
-                    textAlign: "right"
-                })
-            ),
-            $(go.Panel, "Vertical", {
-                    width: 150,
-                    defaultStretch: go.GraphObject.None
-                },
-                $(go.TextBlock, {
-                    text: "alignment: Left",
-                    background: "lightgreen",
-                    margin: 2,
-                    alignment: go.Spot.Left
-                }),
-                $(go.TextBlock, {
-                    text: "alignment: Center",
-                    background: "lightgreen",
-                    margin: 2,
-                    alignment: go.Spot.Center
-                }),
-                $(go.TextBlock, {
-                    text: "alignment: Right",
-                    background: "lightgreen",
-                    margin: 2,
-                    alignment: go.Spot.Right
-                })
-            )
+        $(go.Part, "Vertical",
+            $(go.TextBlock, {
+                text: "a Text Block\nwith three logical lines\nof text",
+                background: "lightgreen",
+                // padding: 5,
+                margin: 20,
+                isMultiline: false
+            }),
+            $(go.TextBlock, {
+                text: "a Text Block\nwith three logical lines\nof text",
+                background: "lightgreen",
+                margin: 2,
+                isMultiline: true
+            }),
+            $(go.TextBlock, {
+                text: "a Text Block\nwith three logical lines\nof centered text",
+                background: "lightgreen",
+                margin: 2,
+                isMultiline: true,
+                textAlign: "center"
+            }),
+            $(go.TextBlock, {
+                text: "a single line of centered text that should wrap because we will limit the width",
+                background: "lightgreen",
+                margin: 2,
+                width: 80,
+                wrap: go.TextBlock.WrapFit,
+                textAlign: "center"
+            })
         ));
 
     window.g = {
