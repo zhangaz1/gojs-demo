5a359227840c2da8f71b12a39426796fd5be9277
zhangaz1 <zhangaz1@hotmail.com>
2016-06-29 15:48:31 +0800

    table布局

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -8,40 +8,56 @@ $(function() {
 
     diagram.nodeTemplate =
         $(go.Node, "Auto",
-            $(go.Shape,
-                new go.Binding("figure", "fig"),
-                new go.Binding("fill", "color")),
-            $(go.TextBlock, {
-                    margin: 5,
-                    editable: true
+            $(go.Shape, {
+                fill: $(go.Brush, go.Brush.Linear, {
+                    0: "white",
+                    1: "lightblue"
+                }),
+                stroke: "darkblue",
+                strokeWidth: 2
+            }),
+            $(go.Panel, "Table", {
+                    defaultAlignment: go.Spot.Left,
+                    margin: 4
                 },
-                new go.Binding("text", "key"))
+                $(go.RowColumnDefinition, {
+                    column: 1,
+                    width: 4
+                }),
+                $(go.TextBlock, {
+                        row: 0,
+                        column: 0,
+                        columnSpan: 3,
+                        alignment: go.Spot.Center
+                    }, {
+                        font: "bold 12pt sans-serif"
+                    },
+                    new go.Binding("text", "key")),
+                $(go.TextBlock, "First: ", {
+                    row: 1,
+                    column: 0
+                }),
+                $(go.TextBlock, {
+                        row: 1,
+                        column: 2
+                    },
+                    new go.Binding("text", "prop1")),
+                $(go.TextBlock, "Second: ", {
+                    row: 2,
+                    column: 0
+                }),
+                $(go.TextBlock, {
+                        row: 2,
+                        column: 2
+                    },
+                    new go.Binding("text", "prop2"))
+            )
         );
 
     diagram.model.nodeDataArray = [{
         key: "Alpha",
-        color: "lightblue",
-        fig: "RoundedRectangle"
-    }, {
-        key: "Beta",
-        color: "lightblue",
-        fig: "Ellipse"
-    }, {
-        key: "Gamma",
-        color: "lightblue",
-        fig: "Hexagon"
-    }, {
-        key: "Delta",
-        color: "lightblue",
-        fig: "FramedRectangle"
-    }, {
-        key: "Epsilon",
-        color: "lightblue",
-        fig: "Cloud"
-    }, {
-        key: "Zeta",
-        color: "lightblue",
-        fig: "Procedure"
+        prop1: "value of 'prop1'",
+        prop2: "the other property"
     }];
 
     window.g = {
