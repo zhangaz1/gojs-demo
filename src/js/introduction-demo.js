0d1cc37e13e043e4b629992570871494ab3543c8
zhangaz1 <zhangaz1@hotmail.com>
2016-06-29 16:34:48 +0800

    填充色、边框、背景色

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -10,20 +10,31 @@ $(function() {
         go.Part,
         'Horizontal',
         G(
-            go.Shape,
-            'Rectangle', {
-                width: 40,
-                height: 60,
-                margin: 4,
-                fill: null
+            go.Shape, {
+                width: 100,
+                height: 40,
+                margin: 2,
+                fill: '#394',
+                strokeWidth: 0
             }
         ),
         G(
-            go.Shape,
-            'Ellipse', {
-                desiredSize: new go.Size(40, 60),
-                margin: 4,
-                fill: null
+            go.Shape, {
+                width: 100,
+                height: 40,
+                fill: null,
+                stroke: '#394',
+                strokeWidth: 4
+            }
+        ),
+        G(
+            go.Shape, {
+                width: 100,
+                height: 40,
+                fill: null,
+                stroke: '#439',
+                strokeWidth: 5,
+                background: '#394'
             }
         )
     ));
