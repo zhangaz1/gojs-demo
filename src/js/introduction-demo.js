c3f8ef0be8fcc6e9c06cf45c08684ed12da94f69
zhangaz1 <zhangaz1@hotmail.com>
2016-06-29 15:42:08 +0800

    使用形状figure

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -8,17 +8,40 @@ $(function() {
 
     diagram.nodeTemplate =
         $(go.Node, "Auto",
-            $(go.Shape, "Rectangle",
+            $(go.Shape,
+                new go.Binding("figure", "fig"),
                 new go.Binding("fill", "color")),
             $(go.TextBlock, {
-                    margin: 5
+                    margin: 5,
+                    editable: true
                 },
                 new go.Binding("text", "key"))
         );
 
     diagram.model.nodeDataArray = [{
         key: "Alpha",
-        color: "lightblue"
+        color: "lightblue",
+        fig: "RoundedRectangle"
+    }, {
+        key: "Beta",
+        color: "lightblue",
+        fig: "Ellipse"
+    }, {
+        key: "Gamma",
+        color: "lightblue",
+        fig: "Hexagon"
+    }, {
+        key: "Delta",
+        color: "lightblue",
+        fig: "FramedRectangle"
+    }, {
+        key: "Epsilon",
+        color: "lightblue",
+        fig: "Cloud"
+    }, {
+        key: "Zeta",
+        color: "lightblue",
+        fig: "Procedure"
     }];
 
     window.g = {
