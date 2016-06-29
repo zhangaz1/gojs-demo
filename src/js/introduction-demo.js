0759cb443e73ca5eec735480bd7a04ff99f2b2be
zhangaz1 <zhangaz1@hotmail.com>
2016-06-29 16:26:27 +0800

    使用textBlock

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -1,75 +1,43 @@
 $(function() {
     // For conciseness. See the "Building Parts" intro page for more
-    var $ = go.GraphObject.make;
+    var G = go.GraphObject.make;
 
-    var diagram = $(go.Diagram, 'myDiagramDiv', {
+    var diagram = G(go.Diagram, 'myDiagramDiv', {
         initialContentAlignment: go.Spot.Center
     });
 
-    diagram.nodeTemplate =
-        $(go.Node, "Spot",
-            // the main content:
-            $(go.Panel, "Vertical",
-                $(go.Picture, {
-                        maxSize: new go.Size(50, 50)
-                    },
-                    new go.Binding("source", "img")),
-                $(go.TextBlock, {
-                        margin: new go.Margin(3, 0, 0, 0)
-                    },
-                    new go.Binding("text", "text"),
-                    new go.Binding("stroke", "error", function(err) {
-                        return err ? "red" : "black"
-                    }))
+    diagram.add(
+        G(go.Part,
+            'Horizontal',
+            G(
+                go.TextBlock, {
+                    text: '文本块',
+                    background: 'blue'
+                }
             ),
-            // decorations:
-            $(go.Shape, "TriangleUp", {
-                    alignment: go.Spot.TopLeft,
-                    fill: "yellow",
-                    width: 14,
-                    height: 14,
-                    visible: false
-                },
-                new go.Binding("visible", "info", function(i) {
-                    return i ? true : false;
-                })),
-            $(go.Shape, "StopSign", {
-                    alignment: go.Spot.TopRight,
-                    fill: "red",
-                    width: 14,
-                    height: 14,
-                    visible: false
-                },
-                new go.Binding("visible", "error")
-            ), {
-                toolTip: $(go.Adornment, "Auto",
-                    $(go.Shape, {
-                            fill: "#FFFFCC"
-                        },
-                        new go.Binding("visible", "info", function(i) {
-                            return i ? true : false;
-                        })),
-                    $(go.TextBlock, {
-                            margin: 4
-                        },
-                        new go.Binding("text", "info"))
-                )
-            }
-        );
-
-    diagram.model.nodeDataArray = [{
-        text: "kitten",
-        img: "images/50x40.png",
-        info: ""
-    }, {
-        text: "kitten",
-        img: "images/50x40.png",
-        error: true,
-        info: "shredded curtains"
-    }];
+            G(
+                go.TextBlock, {
+                    text: '文本块颜色',
+                    stroke: '#492'
+                }
+            ),
+            G(
+                go.TextBlock, {
+                    text: '文本块背景',
+                    background: '#492'
+                }
+            ),
+            G(
+                go.TextBlock, {
+                    text: '文本块字体',
+                    font: 'bold 22px serif'
+                }
+            )
+        )
+    );
 
     window.g = {
-        make: $,
+        make: G,
         diagram: diagram
     };
 });
