66e3d329cae970880f4891668df4ce2fe1bc25de
zhangaz1 <zhangaz1@hotmail.com>
2016-06-29 17:22:43 +0800

    link node

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -6,79 +6,47 @@ $(function() {
         initialContentAlignment: go.Spot.Center
     });
 
-    diagram.add(g(
-        go.Part,
+    var node1 = g(
+        go.Node,
+        'Auto',
         g(
-            go.Panel,
-            'Table',
-            g(
-                go.TextBlock, {
-                    text: '顶标题',
-                    row: 0,
-                    column: 0,
-                    columnSpan: 3,
-                    stretch: go.GraphObject.Horizontal,
-                    margin: 2,
-                    background: '#394'
-                }
-            ),
-            g(
-                go.TextBlock, {
-                    text: '侧标题',
-                    row: 1,
-                    column: 0,
-                    rowSpan: 2,
-                    margin: 2,
-                    stretch: go.GraphObject.Vertical,
-                    background: '#394'
-                }
-            ),
-            g(
-                go.TextBlock, {
-                    text: '一行一列',
-                    row: 1,
-                    column: 1,
-                    margin: 2,
-                    background: '#394'
-                }
-            ),
-            g(
-                go.TextBlock, {
-                    text: '一行两列',
-                    row: 1,
-                    column: 2,
-                    margin: 2,
-                    background: '#394'
-                }
-            ),
-            g(
-                go.TextBlock, {
-                    text: '二行一列',
-                    row: 2,
-                    column: 1,
-                    margin: 2,
-                    background: '#394'
-                }
-            ),
-            g(
-                go.TextBlock, {
-                    text: '二行三列',
-                    row: 2,
-                    column: 3,
-                    margin: 2,
-                    background: '#394'
-                }
-            ),
-            g(
-                go.TextBlock, {
-                    text: '结尾',
-                    row: 3,
-                    column: 2,
-                    columnSpan: 2,
-                    margin: 2,
-                    background: '#394'
-                }
-            )
+            go.Shape, {
+                fill: '#493'
+            }
+        ),
+        g(
+            go.TextBlock, {
+                text: 'node1'
+            }
+        )
+    );
+
+    diagram.add(node1);
+
+    var node2 = g(
+        go.Node,
+        'Auto',
+        g(
+            go.Shape, {
+                fill: '#943'
+            }
+        ),
+        g(
+            go.TextBlock, {
+                text: 'node1'
+            }
+        )
+    );
+
+    diagram.add(node2);
+
+    diagram.add(g(
+        go.Link, {
+            fromNode: node1,
+            toNode: node2
+        },
+        g(
+            go.Shape
         )
     ));
 
