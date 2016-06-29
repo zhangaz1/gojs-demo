82895281f81843390ec60bb911f9dc21365ddb37
zhangaz1 <zhangaz1@hotmail.com>
2016-06-29 16:16:20 +0800

    使用alignment角标

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -7,57 +7,65 @@ $(function() {
     });
 
     diagram.nodeTemplate =
-        $(go.Node, "Auto",
-            $(go.Shape, {
-                fill: $(go.Brush, go.Brush.Linear, {
-                    0: "white",
-                    1: "lightblue"
-                }),
-                stroke: "darkblue",
-                strokeWidth: 2
-            }),
-            $(go.Panel, "Table", {
-                    defaultAlignment: go.Spot.Left,
-                    margin: 4
+        $(go.Node, "Spot",
+            // the main content:
+            $(go.Panel, "Vertical",
+                $(go.Picture, {
+                        maxSize: new go.Size(50, 50)
+                    },
+                    new go.Binding("source", "img")),
+                $(go.TextBlock, {
+                        margin: new go.Margin(3, 0, 0, 0)
+                    },
+                    new go.Binding("text", "text"),
+                    new go.Binding("stroke", "error", function(err) {
+                        return err ? "red" : "black"
+                    }))
+            ),
+            // decorations:
+            $(go.Shape, "TriangleUp", {
+                    alignment: go.Spot.TopLeft,
+                    fill: "yellow",
+                    width: 14,
+                    height: 14,
+                    visible: false
                 },
-                $(go.RowColumnDefinition, {
-                    column: 1,
-                    width: 4
-                }),
-                $(go.TextBlock, {
-                        row: 0,
-                        column: 0,
-                        columnSpan: 3,
-                        alignment: go.Spot.Center
-                    }, {
-                        font: "bold 12pt sans-serif"
-                    },
-                    new go.Binding("text", "key")),
-                $(go.TextBlock, "First: ", {
-                    row: 1,
-                    column: 0
-                }),
-                $(go.TextBlock, {
-                        row: 1,
-                        column: 2
-                    },
-                    new go.Binding("text", "prop1")),
-                $(go.TextBlock, "Second: ", {
-                    row: 2,
-                    column: 0
-                }),
-                $(go.TextBlock, {
-                        row: 2,
-                        column: 2
-                    },
-                    new go.Binding("text", "prop2"))
-            )
+                new go.Binding("visible", "info", function(i) {
+                    return i ? true : false;
+                })),
+            $(go.Shape, "StopSign", {
+                    alignment: go.Spot.TopRight,
+                    fill: "red",
+                    width: 14,
+                    height: 14,
+                    visible: false
+                },
+                new go.Binding("visible", "error")
+            ), {
+                toolTip: $(go.Adornment, "Auto",
+                    $(go.Shape, {
+                            fill: "#FFFFCC"
+                        },
+                        new go.Binding("visible", "info", function(i) {
+                            return i ? true : false;
+                        })),
+                    $(go.TextBlock, {
+                            margin: 4
+                        },
+                        new go.Binding("text", "info"))
+                )
+            }
         );
 
     diagram.model.nodeDataArray = [{
-        key: "Alpha",
-        prop1: "value of 'prop1'",
-        prop2: "the other property"
+        text: "kitten",
+        img: "images/50x40.png",
+        info: ""
+    }, {
+        text: "kitten",
+        img: "images/50x40.png",
+        error: true,
+        info: "shredded curtains"
     }];
 
     window.g = {
