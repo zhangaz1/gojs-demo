d1bf654e3a6461208914c1a52c1bf8a70b9d1ca7
zhangaz1 <zhangaz1@hotmail.com>
2016-06-30 09:20:21 +0800

    数据绑定示例

--- src/js/introduction-demo.js
+++ src/js/introduction-demo.js
@@ -1,37 +1,59 @@
 $(function() {
-    // For conciseness. See the "Building Parts" intro page for more
+    var diagram = new go.Diagram('diagram');
     var g = go.GraphObject.make;
 
-    var diagram = g(go.Diagram, 'myDiagramDiv', {
-        initialContentAlignment: go.Spot.Center
-    });
-
-    diagram.nodeTemplate =
-        g(go.Node, "Auto",
-            g(go.Shape, {
-                    figure: "RoundedRectangle",
-                    fill: "white"
-                },
-                new go.Binding("fill", "color")),
-            g(go.TextBlock, {
-                    margin: 5
-                },
-                new go.Binding("text", "key"))
-        );
-
+    diagram.nodeTemplate = g(
+        go.Node,
+        'Auto',
+        g(
+            go.Shape,
+            'RoundedRectangle', {
+                fill: '#fff'
+            },
+            new go.Binding('fill', 'color')
+        ),
+        g(
+            go.TextBlock,
+            new go.Binding('text', 'text')
+        )
+    )
+    diagram.linkTemplate = g(
+        go.Link,
+        g(
+            go.Shape,
+            new go.Binding('stroke', 'color'),
+            new go.Binding('strokeWidth', 'thick')
+        ),
+        g(
+            go.Shape, {
+                toArrow: 'OpenTriangle',
+                fill: null
+            }
+        )
+    )
     var nodeDataArray = [{
-        key: "Alpha",
-        color: "lightblue"
+        key: '1',
+        text: 'hello1',
+        color: '#345'
     }, {
-        key: "Beta",
-        color: "pink"
-    }];
+        key: '2',
+        text: 'hello2',
+        color: '#245'
+    }, {
+        key: '3',
+        text: 'hello3'
+    }]
     var linkDataArray = [{
-        from: "Alpha",
-        to: "Beta"
-    }];
-    diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-
+        from: '1',
+        to: '2',
+        color: '#452',
+        thick: 2
+    }, {
+        from: '2',
+        to: '3',
+        color: '#452',
+        thick: 2
+    }, ]
     diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
 
     window.g = {
