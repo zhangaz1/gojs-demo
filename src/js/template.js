945a6851b146a9bc3330ef9723ce1f113e799d40
zhangaz1 <zhangaz1@hotmail.com>
2016-08-10 16:31:25 +0800

    简化template代码

--- src/js/template.js
+++ src/js/template.js
@@ -7,33 +7,34 @@ $(function() {
         "undoManager.isEnabled": true
     });
 
-    diagram.model = new go.GraphLinksModel(
-        [{
-            key: "Alpha",
-            color: "lightblue"
-        }, {
-            key: "Beta",
-            color: "orange"
-        }, {
-            key: "Gamma",
-            color: "lightgreen"
-        }, {
-            key: "Delta",
-            color: "pink"
-        }], [{
-            from: "Alpha",
-            to: "Beta"
-        }, {
-            from: "Alpha",
-            to: "Gamma"
-        }, {
-            from: "Beta",
-            to: "Beta"
-        }, {
-            from: "Gamma",
-            to: "Delta"
-        }, {
-            from: "Delta",
-            to: "Alpha"
-        }]);
+    diagram.model.nodeDataArray = [{
+        key: "Alpha",
+        color: "lightblue"
+    }, {
+        key: "Beta",
+        color: "orange"
+    }, {
+        key: "Gamma",
+        color: "lightgreen"
+    }, {
+        key: "Delta",
+        color: "pink"
+    }];
+
+    diagram.model.linkDataArray = [{
+        from: "Alpha",
+        to: "Beta"
+    }, {
+        from: "Alpha",
+        to: "Gamma"
+    }, {
+        from: "Beta",
+        to: "Beta"
+    }, {
+        from: "Gamma",
+        to: "Delta"
+    }, {
+        from: "Delta",
+        to: "Alpha"
+    }];
 });
