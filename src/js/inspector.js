03dd2050be107c5590cc8a78486a1bad5fc0c59c
zhangaz1 <zhangaz1@hotmail.com>
2016-08-10 16:34:31 +0800

    添加inspector

---
+++ src/js/inspector.js
@@ -0,0 +1,40 @@
+$(function() {
+
+    var $ = go.GraphObject.make;
+
+    var diagram = $(go.Diagram, "myDiagramDiv", {
+        initialContentAlignment: go.Spot.Center,
+        "undoManager.isEnabled": true
+    });
+
+    diagram.model.nodeDataArray = [{
+        key: "Alpha",
+        color: "lightblue"
+    }, {
+        key: "Beta",
+        color: "orange"
+    }, {
+        key: "Gamma",
+        color: "lightgreen"
+    }, {
+        key: "Delta",
+        color: "pink"
+    }];
+
+    diagram.model.linkDataArray = [{
+        from: "Alpha",
+        to: "Beta"
+    }, {
+        from: "Alpha",
+        to: "Gamma"
+    }, {
+        from: "Beta",
+        to: "Beta"
+    }, {
+        from: "Gamma",
+        to: "Delta"
+    }, {
+        from: "Delta",
+        to: "Alpha"
+    }];
+});
