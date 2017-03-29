ec611ed80d4940dd33ea3d42ef4165696a70dec5
zhangaz1 <zhangaz1@hotmail.com>
2017-03-29 13:45:55 +0800

    add toSvg

---
+++ src/js/toSvg.js
@@ -0,0 +1,45 @@
+$(function() {
+	initDiagram();
+
+	return void(0);
+
+	function initDiagram() {
+		var $ = go.GraphObject.make;
+
+		var diagram = $(go.Diagram, 'myDiagramDiv', {
+			initialContentAlignment: go.Spot.Center,
+			'undoManager.isEnabled': true
+		});
+
+		diagram.model.nodeDataArray = [{
+			key: 'Alpha',
+			color: 'lightblue'
+		}, {
+			key: 'Beta',
+			color: 'orange'
+		}, {
+			key: 'Gamma',
+			color: 'lightgreen'
+		}, {
+			key: 'Delta',
+			color: 'pink'
+		}];
+
+		diagram.model.linkDataArray = [{
+			from: 'Alpha',
+			to: 'Beta'
+		}, {
+			from: 'Alpha',
+			to: 'Gamma'
+		}, {
+			from: 'Beta',
+			to: 'Beta'
+		}, {
+			from: 'Gamma',
+			to: 'Delta'
+		}, {
+			from: 'Delta',
+			to: 'Alpha'
+		}];
+	}
+});
