c4bee0040e79231ef6b28c56d2f3d27fbf2aa040
zhangaz1 <zhangaz1@hotmail.com>
2017-03-29 13:53:38 +0800

    update template

--- src/js/template.js
+++ src/js/template.js
@@ -5,41 +5,52 @@ $(function() {
 
 	function initDiagram() {
 		var $ = go.GraphObject.make;
+		var diagram = makeDiagram();
+		diagram.model.nodeDataArray = getNodes();
+		diagram.model.linkDataArray = getLinks();
 
-		var diagram = $(go.Diagram, 'myDiagramDiv', {
-			initialContentAlignment: go.Spot.Center,
-			'undoManager.isEnabled': true
-		});
+		return void(0);
 
-		diagram.model.nodeDataArray = [{
-			key: 'Alpha',
-			color: 'lightblue'
-		}, {
-			key: 'Beta',
-			color: 'orange'
-		}, {
-			key: 'Gamma',
-			color: 'lightgreen'
-		}, {
-			key: 'Delta',
-			color: 'pink'
-		}];
+		function getLinks() {
+			return [{
+				from: 'Alpha',
+				to: 'Beta'
+			}, {
+				from: 'Alpha',
+				to: 'Gamma'
+			}, {
+				from: 'Beta',
+				to: 'Beta'
+			}, {
+				from: 'Gamma',
+				to: 'Delta'
+			}, {
+				from: 'Delta',
+				to: 'Alpha'
+			}];
+		}
 
-		diagram.model.linkDataArray = [{
-			from: 'Alpha',
-			to: 'Beta'
-		}, {
-			from: 'Alpha',
-			to: 'Gamma'
-		}, {
-			from: 'Beta',
-			to: 'Beta'
-		}, {
-			from: 'Gamma',
-			to: 'Delta'
-		}, {
-			from: 'Delta',
-			to: 'Alpha'
-		}];
+		function getNodes() {
+			return [{
+				key: 'Alpha',
+				color: 'lightblue'
+			}, {
+				key: 'Beta',
+				color: 'orange'
+			}, {
+				key: 'Gamma',
+				color: 'lightgreen'
+			}, {
+				key: 'Delta',
+				color: 'pink'
+			}];
+		}
+
+		function makeDiagram() {
+			return $(go.Diagram, 'myDiagramDiv', {
+				initialContentAlignment: go.Spot.Center,
+				'undoManager.isEnabled': true
+			});
+		}
 	}
 });
