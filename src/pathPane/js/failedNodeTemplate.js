88748dd4b81953eddff18195fecd04fad4d73c0e
zhangaz1 <zhangaz1@hotmail.com>
2018-11-21 15:30:55 +0800

    clear failed/balance template

--- src/pathPane/js/failedNodeTemplate.js
+++ src/pathPane/js/failedNodeTemplate.js
@@ -18,45 +18,24 @@
             'Auto', {
                 name: 'failedNode',
                 locationSpot: go.Spot.Center,
-                padding: new go.Margin(1, 0, 1, 0),
-                background: nodeConfig.borderColor,
-
-                width: nodeConfig.width,
             },
             new go.Binding('location', 'location', go.Point.parse),
 
-            createDeviceNameTemplate(),
+            createFailedIconTemplate(),
         );
 
         // return void(0);
 
-        function createDeviceNameTemplate() {
+        function createFailedIconTemplate() {
             return $(
-                go.Panel, 'Horizontal', {
-                    name: 'failedNamePanel',
-                    alignment: go.Spot.Left,
-                    height: 50,
-                    margin: new go.Margin(0, 0, 0, 18),
+                go.Picture, {
+                    name: 'failedIcon',
+                    portId: 'failed',
+                    width: 16,
+                    height: 16,
+                    source: failedConfig.icon,
                 },
-                $(
-                    go.Picture, {
-                        name: 'failedIcon',
-                        portId: 'failed',
-                        width: 16,
-                        height: 16,
-                        source: failedConfig.icon,
-                    },
-                    new go.Binding('source', 'icon'),
-                ),
-                $(
-                    go.TextBlock, {
-                        name: 'failedHostNameText',
-                        font: 'bold 12px sans-serif',
-                        stroke: '#111',
-                        isMultiline: false,
-                    },
-                    new go.Binding('text', 'name').makeTwoWay(),
-                ),
+                new go.Binding('source', 'icon'),
             );
         }
 
