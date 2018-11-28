10558afdbc09334690299c43900745cd2c1faa86
zhangaz1 <zhangaz1@hotmail.com>
2018-11-28 15:17:28 +0800

    update rangeLinkLayout

--- src/pathPane/js/rangeLinkLayout.js
+++ src/pathPane/js/rangeLinkLayout.js
@@ -8,10 +8,11 @@
 
     function layout(link) {
         var deviceConfig = link.data.deviceConfig;
-        var portHeight = deviceConfig.topoTypeBase.height +
+        var topoTypeWidth = deviceConfig.topoTypeBase.width;
+        var topoTypeHeight = deviceConfig.topoTypeBase.height +
             deviceConfig.topoTypesPanel.group.height;
         var firstLength = 20; // 第一段、最后一段link长度
-        var secondOffsetY = portHeight + firstLength;
+        var secondOffsetY = topoTypeHeight + firstLength;
 
         updatePoints();
 
@@ -19,26 +20,45 @@
 
         function updatePoints() {
             var points = link.points.toArray();
+            var pointsLength = points.length;
 
-            points[0].y -= portHeight;
+            points[0].y -= topoTypeHeight;
             points[1].y -= secondOffsetY;
 
             points[2].x -= getFromOffsetLeft(link);
             points[2].y = points[1].y;
 
-            points[4].y += secondOffsetY;
-            points[5].y += portHeight;
+            points[pointsLength - 2].y += secondOffsetY;
+            points[pointsLength - 1].y += topoTypeHeight;
 
-            points[3].x -= getToOffsetLeft(link);
-            points[3].y = points[4].y;
+            points[pointsLength - 3].x -= getToOffsetLeft(link);
+            points[pointsLength - 3].y = points[pointsLength - 2].y;
         }
 
         function getFromOffsetLeft(link) {
-            return 85;
+            var fromTopoTypeIndex = getTopoTypeIndex(
+                link.fromNode.data.topoTypes,
+                link.fromPortId
+            );
+            return calculateOffsetX(fromTopoTypeIndex);
         }
 
         function getToOffsetLeft(link) {
-            return 65;
+            var toTopoTypeIndex = getTopoTypeIndex(
+                link.toNode.data.topoTypes,
+                link.toPortId
+            );
+            return calculateOffsetX(toTopoTypeIndex);
+        }
+
+        function getTopoTypeIndex(topoTypes, portId) {
+            return _.findIndex(topoTypes, function(topoType) {
+                return topoType.id === portId
+            });
+        }
+
+        function calculateOffsetX(topoTypeIndex) {
+            return topoTypeWidth * (topoTypeIndex + 1);
         }
     }
 
